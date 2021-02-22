ee38733a65145d85c608d1cff2c1c321d1a0eb95
acscharf <a-scharf@globis.co.jp>
2021-02-23 00:45:29 +0900

    aaa

---
+++ iot-labs/iot-lab-2/frontend_tutorial/bt_car.py
@@ -0,0 +1,51 @@
+import bluetooth
+import threading
+import picar_4wd as fc
+import time
+
+def start_server():
+    myMACAddress = "DC:A6:32:8C:F7:C9" #This bluetooth adapter
+    port = 0
+    backlog = 1
+    size = 1024
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.bind((myMACAddress, port))
+    s.listen(backlog)
+    print("listening on port ", port)
+    try:
+        client, clientInfo = s.accept()
+        while 1:   
+            print("server recv from: ", clientInfo)
+            data = client.recv(size)
+            if data:
+                print(data)
+                distance = fc.get_distance_at(int(data))
+                client.send(str(distance)) # Echo back to client
+    except: 
+        print("Closing socket")
+        client.close()
+        s.close()
+
+def start_client():
+    ubuntuMACAddress = "00:1A:7D:DA:71:15" #
+    port = 1
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((ubuntuMACAddress, port))
+    while 1:
+        text = "yolo"
+        sock.send(text)
+        data = sock.recv(1024)
+        time.sleep(1.5)
+
+    sock.close()
+
+sth = threading.Thread(target=start_server)
+cth = threading.Thread(target=start_client)
+
+sth.start()
+cth.start()
+
+cth.join()
+sth.join()
+
+print("Success, terminating")
---
+++ iot-labs/iot-lab-2/frontend_tutorial/bt_ubuntu.py
@@ -0,0 +1,50 @@
+import bluetooth
+import threading
+
+def start_server():
+    myMACAddress = "00:1A:7D:DA:71:15" #This bluetooth adapter
+    port = 0
+    backlog = 1
+    size = 1024
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.bind((myMACAddress, port))
+    s.listen(backlog)
+    print("listening on port ", port)
+    try:
+        client, clientInfo = s.accept()
+        while 1:   
+            print("server recv from: ", clientInfo)
+            data = client.recv(size)
+            if data:
+                print(data)
+    except: 
+        print("Closing socket")
+        client.close()
+        s.close()
+
+def start_client():
+    carMACAddress = "DC:A6:32:8C:F7:C9" # The address of Raspberry PI Bluetooth adapter on the server.
+    port = 1
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((carMACAddress, port))
+    while 1:
+        text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
+        if text == "quit":
+            break
+        sock.send(text)
+
+        data = sock.recv(1024)
+        print("from server: ", data)
+
+    sock.close()
+
+sth = threading.Thread(target=start_server)
+cth = threading.Thread(target=start_client)
+
+sth.start()
+cth.start()
+
+cth.join()
+sth.join()
+
+print("Success, terminating")
