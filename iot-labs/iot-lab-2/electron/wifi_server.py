4c847271e29c6846d8beb643eea03cac93ad96ad
acscharf <acscharf@gmail.com>
2021-02-24 23:46:59 +0900

    sdsd

--- iot-labs/iot-lab-2/electron/wifi_server.py
+++ iot-labs/iot-lab-2/electron/wifi_server.py
@@ -11,34 +11,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
-    try:
-        while 1:
-            client, clientInfo = s.accept()
-            print("server recv from: ", clientInfo)
-            data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            if data != b"":
-                command = data.decode(encoding='UTF-8')
-                if command:
-                    if command == 'w':
-                        fc.forward(power_val)
-                        client.send("Moving forward")
-                    elif command == 'a':
-                        fc.turn_left(power_val)
-                        client.send("Turning left")
-                    elif command == 's':
-                        fc.backward(power_val)
-                        client.send("Moving back")
-                    elif command == 'd':
-                        fc.turn_right(power_val)
-                        client.send("Turning right")
-                    elif command == 'stop':
-                        fc.stop()
-                        client.send("Stopping")
-                    else:
-                        print("Unrecognized command")
-                        print(data)
-                        print(type(data))
+    #try:
+    while 1:
+        client, clientInfo = s.accept()
+        print("server recv from: ", clientInfo)
+        data = client.recv(1024)      # receive 1024 Bytes of message in binary format
+        if data != b"":
+            command = data.decode(encoding='UTF-8')
+            if command:
+                if command == 'w':
+                    fc.forward(power_val)
+                    client.send("Moving forward")
+                elif command == 'a':
+                    fc.turn_left(power_val)
+                    client.send("Turning left")
+                elif command == 's':
+                    fc.backward(power_val)
+                    client.send("Moving back")
+                elif command == 'd':
+                    fc.turn_right(power_val)
+                    client.send("Turning right")
+                elif command == 'stop':
+                    fc.stop()
+                    client.send("Stopping")
+                else:
+                    print("Unrecognized command")
+                    print(data)
+                    print(type(data))
+    '''
     except: 
         print("Closing socket")
         client.close()
-        s.close()    
+        s.close()
+        '''    
